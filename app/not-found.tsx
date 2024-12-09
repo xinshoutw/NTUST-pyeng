e3fe7a7e3131da34390744ba1953bbbf97638582
XinShou <icexinshou@gmail.com>
2024-12-09 15:50:08 +0800

    style: formatting

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,12 @@
 export const runtime = 'edge';
 
-import { HTTPAccessErrorFallback } from './error-fallback'
+import {HTTPAccessErrorFallback} from './error-fallback'
 
 export default function NotFound() {
-  return (
-    <HTTPAccessErrorFallback
-      status={404}
-      message="This page could not be found."
-    />
-  )
+    return (
+        <HTTPAccessErrorFallback
+            status={404}
+            message="This page could not be found."
+        />
+    )
 }
