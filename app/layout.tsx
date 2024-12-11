f96597997d9eeb1ca22ed58bda85da7d26ee0344
XinShou <icexinshou@gmail.com>
2024-12-11 15:53:50 +0800

    fix: home page button not working

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,10 @@ export default async function RootLayout({children}: { children: ReactNode }) {
             className="bg-gray-50 text-gray-800 dark:bg-gray-900 dark:text-gray-100 min-h-screen flex flex-col transition-colors duration-300">
         <header className="bg-white dark:bg-gray-800 shadow fixed top-0 w-full z-50">
             <div className="max-w-5xl mx-auto p-4 flex items-center justify-between">
-                <Link href="/" prefetch={false}
-                      className="flex items-center font-bold text-lg text-gray-800 dark:text-gray-100 hover:text-gray-600 dark:hover:text-gray-300 transition-colors">
+                {/* unfixable issue, use <a> instead of <Link> */}
+                {/* ref: https://github.com/vercel/next.js/discussions/57565 */}
+                <a href="/"
+                   className="flex items-center font-bold text-lg text-gray-800 dark:text-gray-100 hover:text-gray-600 dark:hover:text-gray-300 transition-colors">
                     <Image
                         src="/android-chrome-192x192.png"
                         alt="Icon"
@@ -34,7 +36,7 @@ export default async function RootLayout({children}: { children: ReactNode }) {
                         priority={true}
                     />
                     NTUST 英簡單
-                </Link>
+                </a>
                 <nav className="space-x-4 flex items-center">
                     <Link href="https://pygit.xserver.tw/"
                           className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition">
