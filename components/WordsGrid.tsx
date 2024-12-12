a6e6419e5bfb2a810b2cd6131e621402859601a3
XinShou <icexinshou@gmail.com>
2024-12-13 02:14:15 +0800

    fix: wrong behavior

--- components/WordsGrid.tsx
+++ components/WordsGrid.tsx
@@ -76,7 +76,6 @@ export default function WordsGrid({
             return;
         }
 
-
         loadData(lastPart, lastTopic, selectedPart, selectedTopic);
     }, [selectedPart, selectedTopic]);
 
@@ -102,16 +101,18 @@ export default function WordsGrid({
             setWords(wordData.words);
 
             if (_last_part != _part) {
-                const partData = await fetchParts(topic);
-                const newPartOptions = parseParts(partData);
-                setCookie('lastPart', String(selectedPart));
-                setParts(newPartOptions);
-            }
-            if (_last_topic !== _topic) {
                 const topicData = await fetchTopics(part);
                 const newTopicOptions = parseTopics(topicData);
-                setCookie('lastTopic', String(selectedTopic));
                 setTopics(newTopicOptions);
+
+                setCookie('lastPart', String(selectedPart));
+            }
+            if (_last_topic !== _topic) {
+                const partData = await fetchParts(topic);
+                const newPartOptions = parseParts(partData);
+                setParts(newPartOptions);
+
+                setCookie('lastTopic', String(selectedTopic));
             }
         } catch (error) {
             console.error("Failed to load data:", error);
